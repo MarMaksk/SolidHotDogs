2bfe7148f689bd4456e969497aa54b82301cb11c
MarMaksk <87898650+MarMaksk@users.noreply.github.com>
2021-09-24 16:56:19 +0300

    Make method save to file work

--- src/com/company/HotDogs.java
+++ src/com/company/HotDogs.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.io.*;
+
 public abstract class HotDogs implements HotDog {
 
-    private void compositionToFile(String composition) {
+    private File file = new File("compositionHD.txt");
 
+    private void compositionToFile(String composition) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+            file.createNewFile();
+            bw.write(composition);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
